c85e175e860d7a2a45497a5aff4b5332e9fcee2c
felipe26x <dfvalencia14@misena.edu.co>
2022-10-20 19:52:06 -0500

    new

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 
-var images = ["duvacho 1.png", "duva.jpg", "imagen m8.png"]
+
+var vendedor = prompt("ingresa el vendedor")
+var clientes = [];
 
 
-var num = 0;
-
-function next(){
-    var slider = document.getElementById("slider");
-    num++;
-
-    if(num >= images.length){
-        num = 0;
 
 
-    }
-    slider.src = images[num]
-
-}
-
-
-function prev(){
-    var slider = document.getElementById("slider");
-    num--;
-
-    if(num < 0){
-
-        num = images.length-1;
-
-
-    }
-    slider.src = images[num]
-
+function pr(){
+   
+     
 }
